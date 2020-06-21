4a80435432a624e1857c8bd0ce24092d5dd9e1a7
Aleksandr <aleksandros-777@mail.ru>
2020-06-21 08:25:35 +0300

    refactored task_2

--- Task2_solved.py
+++ Task2_solved.py
@@ -6,33 +6,32 @@ import numpy as np
 import argparse
 
 
-def get_data_from_web_and_parses_it():
+def get_data_from_web():
     """Sends a request to web, gets a result and parses it"""
-    print()
-    try:
-        response = requests.get(
-            'http://ir.eia.gov/ngs/wngsr.json')
-        response = response.content.decode(response.apparent_encoding)
-        response_json = json.loads(response)
-        net_changes = []
-        for data_series in response_json['series']:
-            name = data_series['name']
-            calculated_net_change = data_series['calculated']['net_change']
-            if 'total' in name:
-                print(f'Total net change: {calculated_net_change}')
-            else:
-                net_change = int(data_series['calculated']['net_change'])
-                net_changes.append(net_change)
-        average_net_change = np.average(net_changes)
-        print(f'Average meaning of net changes is {average_net_change}')
-    except ValueError:
-        print('Something went wrong!')
+    response = requests.get('http://ir.eia.gov/ngs/wngsr.json')
+    response_json = json.loads(response.content)
+    return response_json
+
+
+def parse_json_data_from_web(data):
+    """Parses received from the WEB JSON-data"""
+    net_changes = []
+    for data_series in data['series']:
+        name = data_series['name']
+        calculated_net_change = data_series['calculated']['net_change']
+        if 'total' in name:
+            print(f'\nTotal net change: {calculated_net_change}')
+        else:
+            net_change = int(data_series['calculated']['net_change'])
+            net_changes.append(net_change)
+    average_net_change = np.average(net_changes)
+    print(f'Average meaning of net changes is {average_net_change}')
 
 
 def get_time_to_start_and_run_script():
     """Picks start time and starts parser"""
     current_time = datetime.now().time().strftime('%H:%M')
-    start_time = "16:22"
+    start_time = "08:16"
 
     parser = argparse.ArgumentParser()
     parser.add_argument("--starts", help="changes start time")
@@ -45,7 +44,11 @@ def get_time_to_start_and_run_script():
         current_time = datetime.now().time().strftime('%H:%M')
         time.sleep(1)
     else:
-        get_data_from_web_and_parses_it()
+        try:
+            data_json = get_data_from_web()
+            parse_json_data_from_web(data_json)
+        except (ValueError, KeyError):
+            print('Something went wrong!')
 
 
 def main():
